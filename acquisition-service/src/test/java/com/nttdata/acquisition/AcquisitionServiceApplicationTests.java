2545c2715d1f7569a896bcb687356d8cabff397c
Juan Soto <jsotcast@nttdata.com>
2021-11-26 12:20:25 -0500

    refactor add project acquisition-service

---
+++ acquisition-service/src/test/java/com/nttdata/acquisition/AcquisitionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.acquisition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcquisitionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
